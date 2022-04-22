f3a58317669ea3abc7ae859016de57f72ef5a7d5
superstaresn123 <69216910+superstaresn123@users.noreply.github.com>
2022-04-22 11:07:01 -0400

    cool rumba stuff

---
+++ Rumba_libary.h
@@ -0,0 +1,4 @@
+void rumba_drive();
+void rumba_turn_left();
+void rumba_turn_right();
+void rumba_drive_bump();
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <kipr/wombat.h>
+#include "Rumba_libary.h"
+int main(){
+rumba_drive(10000);
+rumba_turn_right(1000);
+return 0;
+}
